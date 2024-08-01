69467835015a673f7bbf4e223ab4e3dfb7da7a69
Anshuman <anshumansharma0118@gmail.com>
2024-08-01 16:18:33 +0530

    daily

---
+++ 10.cpp
@@ -0,0 +1,49 @@
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+
+//     // int A[20]={2,4,6,8,10};
+//     // string S="VIJAY";
+//     // // cout<<S[0];
+//     // cout<<S.size();
+//     // string S="abcdianjyp";
+//     // string substring="ianjyp";
+//     // cout<<S.find(substring);
+//     int Arr[] = {1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0};
+//     int maxi = 0;
+//     int cnt = 0;
+    
+//     for (int i = 0; i < 16; i++)
+//     {
+//         if (Arr[i] == 1)
+//         {
+//             cnt++;
+//         }
+//         else
+//         {
+//             cnt = 0;
+//         }
+//         maxi = max(maxi, cnt);
+//     }
+
+//     return maxi;
+// }
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int Arr[]={1,1,0,1,0,1,1,1,1,0,0,1,0};
+    // int n=13;
+    // for(int i =1;i<n;i++){
+    //     if (Arr[i]==1 and Arr[i-1]==0){
+    //         cout<<"01";
+    //         break;
+    //     }
+    //     if(Arr[i]==0 and Arr[i-1]==1){
+    //         cout<<"10";
+    //         break;
+    //     }
+
+    // }
+    
+}
