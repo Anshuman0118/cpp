ebed031bde07a1b0f4cbd8da1f497dd1c5ca7396
Anshuman <anshumansharma0118@gmail.com>
2024-08-02 16:21:37 +0530

    Daily

--- 11.cpp
+++ 11.cpp
@@ -1,3 +1,53 @@
+// #include<iostream>
+// using namespace std;
+// class students{
+//     private:
+//     string name;
+//     int id;
+//     int arr[5];
+//     int total=0;
+//     public:
+//     students(){
+//         cout<<"input name and id";
+//         cin>>name;
+//         cin>>id;
+
+//         for(int i=0;i<5;i++){
+//             cout<<"enter the marks "<<1<<endl;
+//             cin>>arr[i];
+//             total=total+arr[i];
+//         }
+//         cout<<total/5;
+
+
+//     }
+// };
+// int main(){
+//     students anshuman;
+// // }
+// #include<bits/stdc++.h>
+// using namespace std;
+// class car{
+//     private:
+//     string brand;
+//     string model;
+//     int year;
+//     public:
+//     car(){
+//         cout<<"enter the brand name";
+//         cin>>brand;
+//         cout<<"enter the model name";
+//         cin>>model;
+//         cout<<"enter the year";
+//         cin>>year;
+//         cout<<"brand: "<<brand<<endl;
+//         cout<<"model: "<<model<<endl;
+//         cout<<"year: "<<year<<endl;
+//     }
+// };
+// int main(){
+//     car one;
+// }
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
